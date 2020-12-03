918bb8a1b631dfde9630a12f6606be626646a956
Nick <nick@keysysconsulting.com>
2020-12-03 12:38:53 -0600

    Day 3

---
+++ AdventOfCode/AdventOfCode/2020/Day3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode._2020
+{
+    public class Day3
+    {
+        public int Execute(string input, int xStep = 3, int yStep = 1)
+        {
+            var values = input.Split('&');
+            var length = values[0].Length;
+            var y = yStep;
+            var x = xStep;
+            var trees = 0;
+            do
+            {
+                if (values[y][x] == '#')
+                {
+                    trees++;
+                }
+
+                y += yStep;
+                x += xStep;
+                if (x >= length)
+                {
+                    x -= length;
+                }
+            } while (y < values.Length);
+
+            return trees;
+        }
+    }
+}
